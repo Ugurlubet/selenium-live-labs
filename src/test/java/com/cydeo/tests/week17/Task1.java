f4221bd943ffa96895a3689d390a47ee083f60d4
Ugurlubet <betldzc@gmail.com>
2025-05-07 22:10:58 +0300

    initial project creation

--- src/test/java/com/cydeo/tests/week17/Task1.java
+++ src/test/java/com/cydeo/tests/week17/Task1.java
@@ -1,6 +1,40 @@
 package com.cydeo.tests.week17;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task1 {
+    public static void main(String[] args) {
+        /*
+        *  TC001 As a user I should be able to login with valid credentials
+
+     1- Open a chrome browser
+     2- Go to "https://vytrack.com/"
+     3- Click LOGIN button
+     4- Login to application with username as "User1" and password as "UserUser123"
+     5- Verify the title contains "Dashboard"*/
+
+        WebDriver driver=new ChromeDriver();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
 
 
 
